2041474a7602c6116b9666ee6a47213c9375a336
Владислав <vladislav@MacBook-Air.local>
2020-07-31 20:29:02 +0400

    ver. 0.23

--- PasteRepository.py
+++ PasteRepository.py
@@ -3,15 +3,28 @@ import pymysql
 class PasteRepository:
 
 	def __init__(self, host, user, password, db, port, charset):
-		self.connection = pymysql.connect(host=host, user=user, password=password, db=db, port=port, charset=charset)
+		self.host=host
+		self.user=user
+		self.password=password
+		self.db=db
+		self.port=port
+		self.charset=charset
+		connect()
+
+	def connect(self):
+		self.connection = pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db, port=self.port, charset=self.charset)
 
 	def isPasteExists(self, title):
+		if self.connection.open is not True:
+			connect()
 		with self.connection.cursor() as cursor:
 			query = 'SELECT * FROM paste WHERE title = "%s"'
 			cursor.execute(query, title)
 			return cursor.fetchone() is not None
 
 	def savePaste(self, paste):
+		if self.connection.open is not True:
+			connect()
 		with self.connection.cursor() as cursor:
 			if not self.isPasteExists(paste['title']):
 				query = 'INSERT INTO paste (title, content, rating, popularity, source, url) VALUES (%s, %s, %s, %s, %s, %s)'
@@ -21,6 +34,8 @@ class PasteRepository:
 			return False
 
 	def getRandomPaste(self):
+		if self.connection.open is not True:
+			connect()
 		with self.connection.cursor() as cursor:
 			query = 'SELECT * FROM paste ORDER BY RAND() LIMIT 1'
 			cursor.execute(query)
